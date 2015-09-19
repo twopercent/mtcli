0358ca5713aadd13b00fb368bb6ff406bcdcfadb
Chris Moser <chrismmoser@gmail.com>
2015-09-19 15:17:32 -0500

    finished get directions, getstops, and getdepartures functions

--- mtcli.py
+++ mtcli.py
@@ -12,7 +12,7 @@ import urllib.request
 import xml.etree.ElementTree as ET
 
 def parseDirection (busNum):
-    "This function takes a bus number and returns a dictionary of directions and the direction code"
+    "This function takes a bus number and returns a list of lists of directions and their direction code"
 
     directions = urllib.request.urlopen('http://svc.metrotransit.org/NexTrip/Directions/' + busNum)
     data = directions.read()
@@ -23,14 +23,18 @@ def parseDirection (busNum):
     tree = ET.parse('directions.xml')
     root = tree.getroot()
 
-    result = dict()
+    result = []
 
     for element in root:
-        result[element[0].text] = element[1].text
+        list = []
+        list.append(element[0].text)
+        list.append(element[1].text)
+
+        result.append(list)
     return result
 
 def parseStops (busNum, busDirec):
-    "This function taks a bus number and direction and returns a dictionary of stops and stop codes"
+    "This function taks a bus number and direction code and returns a list of lists of stops and stop codes"
 
     stops = urllib.request.urlopen('http://svc.metrotransit.org/NexTrip/Stops/' + busNum + '/' + busDirec)
     data = stops.read()
@@ -41,49 +45,79 @@ def parseStops (busNum, busDirec):
     tree = ET.parse('stops.xml')
     root = tree.getroot()
 
-    result = dict()
+    result = []
 
     for element in root:
-        result[element[0].text] = element[1].text
+        list = []
+        list.append(element[0].text)
+        list.append(element[1].text)
+
+        result.append(list)
+    
     return result
 
-#bus = input("Please enter your bus number ")
-#busDirections = parseDirection(bus)
+def parseDepartures (busNum, busDirec, busStop):
+    "This function taks a bus number, direction code, and stop code and returns a list of departures"
 
-#for key in busDirections:
-#    print(key)
+    departures = urllib.request.urlopen('http://svc.metrotransit.org/NexTrip/' + busNum + '/' + busDirec + '/' + busStop)
+    data = departures.read()
+    f = open('departures.xml', 'wb')
+    f.write(data)
+    f.close()
 
-#direction = input("What direction?\t") 
+    tree = ET.parse('departures.xml')
+    root = tree.getroot()
 
-busStops = parseStops("540", "2")
-for key in busStops:
-    print(key)
+    result = []
 
+    for element in root:
+        list = []
+        list.append(element[2].text)
+        list.append(element[6].text)
+        list.append(element[4].text)
+        list.append(element[1].text)
 
-#directions = urllib.request.urlopen('http://svc.metrotransit.org/NexTrip/Directions/' + busNum)
-#data = directions.read()
-#f = open('directions.xml', 'wb')
-#f.write(data)
-#f.close()
+        result.append(list)
+    
+    return result
 
-#tree = ET.parse('directions.xml')
-#root = tree.getroot()
+bus = input("Please enter your bus number ")
+busDirections = parseDirection(bus)
 
-#print("\nYou chose bus number " + bus)
+index = 0
+print()
 
+for item in busDirections:
+    print(str(index) + ". " + str(item[0]))
+    index += 1
 
+print()
 
-#tree = ET.parse('directions.xml')
-#root = tree.getroot()
+direction = input("Please enter your direction ")
+directionKey = busDirections[int(direction)][1]
+busStops = parseStops(bus, directionKey)
 
-#print("\nNow for child in root, print child.tag and child.attrib")
-#for child in root:
-#    print (child.tag, child.attrib)
+index = 0
+print()
 
-#print("\nfor elem in iter on tree")
-#iter_ = tree.getiterator()
-#for elem in iter_:
-#    print(elem.tag)
+for item in busStops:
+    print(str(index) + ". " + str(item[0]))
+    index += 1
+
+print()
+
+stop = input("Plese enter your stop ")
+stopKey = busStops[int(stop)][1]
+busDepartures = parseDepartures(bus, directionKey, stopKey)
+
+print()
+print("Upcoming departures: \n")
+
+for item in busDepartures:
+    print(str(item[0]) + " " + str(item[1]) + " " + str(item[2]) + " " + str(item[3]))
+
+print()
+
 
 #appointments = root.getchildren()
 #print ("\nfor child in children on tree.getchildren()")
@@ -91,13 +125,3 @@ for key in busStops:
 #    appt_children = appointment.getchildren()
 #    for appt_child in appt_children:
 #        print(appt_child.tag, appt_child.text)
-
-#stops = urllib.request.urlopen('http://svc.metrotransit.org/NexTrip/Stops/' + busNum + '/2')
-#data = stops.read()
-#f = open('stops.xml', 'wb')
-#f.write(data)
-#f.close()
-
-#tree = ET.parse('stops.xml')
-#root = tree.getroot()
-
