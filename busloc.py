8b25aaf4777becc89ee69d42ac8898730bdc38b1
Chris Moser <chrismmoser@gmail.com>
2015-10-01 17:31:33 -0500

    added comments

--- busloc.py
+++ busloc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.4
+#!/usr/local/bin/python3
 #
 # A command line tool to get NextTrip information
 #
@@ -13,18 +13,42 @@ import xml.etree.ElementTree as ET
 import xml.dom.minidom as MD
 
 def getlocations (busNum):
-    "This function takes a route number and returns a CSV list of their locations"
+    "This function takes a route number and returns a GeoJSON Multipoint array of vehicle locations"
 
     locations = urllib.request.urlopen('http://svc.metrotransit.org/NexTrip/VehicleLocations/' + busNum)
     data = locations.read()
+    root = ET.fromstring(data)
+
+    print("This is what I think the answer should be")
+    for vehicle in root:
+        lat = vehicle.find('VehicleLatitude')
+        lon = vehicle.find('VehicleLongitude').text
+        print('lat lon are ' + str(lat) + str(lon))
+    
+    print("I know the data is there because I can access it")
+    for vehicle in root:
+        lat = vehicle[8].text
+        lon = vehicle[9].text
+        print('lat lon by index ' + str(lat) + str(lon))
+
+    print("I think 'find' should work because vehicle is an ElementObject")
+    for vehicle in root:
+        print('vehicle type: ' + str(type(vehicle)))
+        
+def printXML (busNum):
+    "This function takes a bus number and prints the XML location data nicely for viewing"
+    locations = urllib.request.urlopen('http://svc.metrotransit.org/NexTrip/VehicleLocations/' + busNum)
+    data = locations.read()
+
     f = open('locations.xml', 'wb')
     f.write(data)
     f.close()
 
     xml = MD.parse('locations.xml')
-    pretty_xml = xml.toprettyxml()
+    print(xml.toprettyxml())
 
-    print(pretty_xml)
 
-bus = input('Please enter bus number or \"0\" for all: ')
+#bus = input('Please enter bus number or \"0\" for all: ')
+bus = '444'
+printXML(bus)
 getlocations(bus)
