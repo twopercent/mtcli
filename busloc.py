73608fb31795e7a28058f76e07fc5e3df27e6f97
Chris Moser <chrismmoser@gmail.com>
2015-10-02 11:47:23 -0500

    Got some output in json form but cannot get to a MultiPoint yet

--- busloc.py
+++ busloc.py
@@ -11,9 +11,40 @@
 import urllib.request
 import xml.etree.ElementTree as ET
 import xml.dom.minidom as MD
+#from geojson import MultiPoint
+import geojson
+#import json
 
 def getlocations (busNum):
-    "This function takes a route number and returns a GeoJSON Multipoint array of vehicle locations"
+    "This function takes a route number and writes a GeoJSON Multipoint array of vehicle locations"
+
+    locations = urllib.request.urlopen('http://svc.metrotransit.org/NexTrip/VehicleLocations/' + busNum)
+    root = ET.fromstring(locations.read())
+
+    f = open('vehicles.geojson', 'wb')
+    
+    mp_list = []
+
+    for vehicle in root:
+        point = {
+            'type': 'MultiPoint',
+            'coordinates': [[vehicle[9].text], [vehicle[8].text]]
+        }
+        mp_list.append(point)
+
+    mp_thing = geojson.dumps(mp_list)
+    #json.dumps(mp_list, f)
+    print(mp_thing)
+
+    #f.write('{ "type": "MultiPoint",\n\t"coordinates": [ ')
+    #for vehicle in root:
+    #    f.write("[" + vehicle[8].text + ", " + vehicle[9].text + "], ")
+    #f.write(' ]\n\t]\n}')
+
+    f.close()
+
+def getlocationstest (busNum):
+    "This function takes a route number and writes a GeoJSON Multipoint array of vehicle locations"
 
     locations = urllib.request.urlopen('http://svc.metrotransit.org/NexTrip/VehicleLocations/' + busNum)
     data = locations.read()
@@ -26,8 +57,21 @@ def getlocations (busNum):
         print('lat lon are ' + str(lat) + str(lon))
 
     print("This is a test section")
-    for vehicle in root.iter('Route'):
-        print(vehicle.text)
+    for vehicle0 in root.iter('Route'):
+        print("root.iter " + str(type(vehicle0)))
+    
+    for vehicle1 in root:
+        data = vehicle1.find('Route')
+        print("vehicle1.find " + str(type(data)))
+    
+    for vehicle2 in root:
+        data = vehicle2.get('Route')
+        print("vehicle2.get " + str(type(data)))
+
+    for vehicle3 in root:
+        data = vehicle3.findall('Route')
+        print("vehicle3.findall " + str(type(data)))
+
 
     print("I know the data is there because I can access it")
     for vehicle in root:
